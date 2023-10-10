1c013634eed26a5f06449b519996b75d4d5865da
Adiparida <adityaanshumanparida@gmail.com>
2023-10-10 13:50:51 +0530

    HIS-301:CHANGES

--- src/main/java/com/adi/Application.java
+++ src/main/java/com/adi/Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+	int j=1;
+	int k = 2;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
